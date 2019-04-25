bf23a2cac18819be1f212b58a7cbc18d79659d27
Allan Gao <allangao95@gmail.com>
2019-04-25 14:22:06 +1000

    team leader stats rendered

--- src/components/teams/teamPageCards/TeamLeadersCard.js
+++ src/components/teams/teamPageCards/TeamLeadersCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { endpointConstants } from "./../../../api/endpoints";
 import "./TeamLeadersCard.css";
 
 class TeamLeadersCard extends Component {
@@ -6,34 +7,57 @@ class TeamLeadersCard extends Component {
     return this.props.allPlayers.find(player => player.personId === personId);
   };
 
-  //   renderTeamLeaders = list => {
+  //   renderTeamLeaders = (list, stat) => {
+  //     // const playerImg = endpointConstants.FETCH_PLAYER_HEADSHOT(player.personId);
 
-  //   }
+  //     // const playerImageStyle = {
+  //     //   // backgroundImage: `linear-gradient(
+  //     //   //         rgba(252,252,252,0.6),
+  //     //   //         rgb(254, 254, 254,0.7)
+  //     //   //       ), url(${logoImg}) `,
+  //     //   maxWidth: "100%",
+  //     //   height: "auto",
+  //     //   backgroundSize: "cover",
+  //     //   backgroundPosition: "center"
+  //     // };
+
+  //     return (
+  //       <div className="player-cards-container">
+  //         <li key={stat}>{list[0].stat[0].value}</li>;
+  //       </div>
+  //     );
+  //   };
 
   render() {
     const teamLeaders = this.props.teamLeaders;
-    // console.log(teamLeaders);
-    const players = this.props.allPlayers;
+    console.log(teamLeaders);
+    // const players = this.props.allPlayers;
     // console.log(players);
 
     console.log(this.getPlayerName(teamLeaders.ppg[0].personId).fullName);
     console.log(teamLeaders.ppg[0].value);
-    const STAT_NAME = {
-      ppg: "PTS",
-      trpg: "REB",
-      apg: "AST",
-      bpg: "BLK",
-      spg: "STL"
-    };
+
+    const stats = ["ppg", "trpg", "apg", "spg", "bpg"];
+
     return (
       <div className="team-leaders-card">
-        {/* <h1>
-          {this.getPlayerName(teamLeaders.ppg[0].personId)
-            .temporaryDisplayName.replace(",", " ")
-            .reverse()}
-        </h1> */}
-        {/* <h1>{teamLeaders.ppg[0].personId}</h1>
-        <h1>{teamLeaders.ppg[0].value}</h1> */}
+        {stats.map(stat => {
+          console.log(teamLeaders[stat][0].value);
+          const leadPlayer = this.getPlayerName(teamLeaders[stat][0].personId)
+            .fullName;
+          const leadStat = teamLeaders[stat][0].value;
+          const playerImg = endpointConstants.FETCH_PLAYER_HEADSHOT(
+            teamLeaders[stat][0].personId
+          );
+
+          return (
+            <div>
+              <img src={playerImg} alt="stat leader" />
+              <h1>{leadPlayer}</h1>
+              <h1>{leadStat}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
