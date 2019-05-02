15e680014b7bde4fd3a1fbecb290ee0686982a2a
Allan Gao <allangao95@gmail.com>
2019-05-02 14:44:31 +1000

    heroku proxy

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-const express = require("express");
-const proxy = require("http-proxy-middleware");
-const app = express();
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || "0.0.0.0";
+// Listen on a specific port via the PORT environment constiable
+const port = process.env.PORT || 8080;
 
-const port = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-app.use(
-  "/proxy",
-  proxy({
-    pathRewrite: {
-      "^/proxy/": "/"
-    },
-    target: "https://server.com",
-    secure: false
+const cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: process.env.CORSANYWHERE_WHITELIST,
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"]
   })
-);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-});
-
-app.listen(port, () => {
-  console.log(path.join(__dirname, "./build/"));
-  console.log(`Listening on ${port}`);
-});
+  .listen(port, host, function() {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
